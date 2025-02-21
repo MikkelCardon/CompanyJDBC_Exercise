faf64ade9b4f7108b792925de97d74bb13f10a57
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-02-21 17:37:55 +0100

    Implementaion of multimple methods

--- src/storage/ConnictionToDB.java
+++ src/storage/ConnictionToDB.java
@@ -11,8 +11,8 @@ public class ConnictionToDB {
 
     static {
         try {
-            minConnection = DriverManager
-                    .getConnection("jdbc:sqlserver://localhost\\SQLExpress;databaseName=JeresDatabase;user=sa;password=JeresPassword;");
+            String url = "jdbc:sqlserver://DESKTOP-KQNO6AD\\SQLEXPRESS;databaseName=CompanyEmployee;user=sa;password=CARDONDB;";
+            minConnection = DriverManager.getConnection(url);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -39,27 +39,76 @@ public class ConnictionToDB {
 
     public static ArrayList<Employee> getEmployee() {
         ArrayList<Employee> emp = new ArrayList<Employee>();
-        //TODO
+        try {
+            Statement statement = minConnection.createStatement();
+            ResultSet res = statement.executeQuery("SELECT * FROM Employees");
+
+            while(res.next()){
+                int id = res.getInt(1);
+                String name = res.getString(2);
+                int wage = res.getInt(3);
+
+                Employee employee = new Employee(id, name, wage);
+                emp.add(employee);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return emp;
     }
 
     public static ArrayList<Employee> getEmpForComp(int companyid) {
         ArrayList<Employee> ansatte = new ArrayList<Employee>();
-       //TODO
+       try{
+           Statement statement = minConnection.createStatement();
+           ResultSet res = statement.executeQuery("SELECT * FROM Employees WHERE companyID = " + companyid);
+
+           while(res.next()){
+               while(res.next()){
+                   int id = res.getInt(1);
+                   String name = res.getString(2);
+                   int wage = res.getInt(3);
+
+                   Employee employee = new Employee(id, name, wage);
+                   ansatte.add(employee);
+               }
+           }
+
+
+       }catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
         return ansatte;
     }
 
     public static String removeCompany(int id) {
-        String s = "slettet";
-       //TODO
-        return s;
+
+        try{
+            Statement statement = minConnection.createStatement();
+            statement.executeUpdate("DELETE FROM Company WHERE companyID = " + id);
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return "Company id:" + id +". Er blever slettet";
     }
 
     //return name of company for employee
     public static String getCompanyForEmp(Employee emp) {
-        Company c = null;
+
         String Cname = "";
-        //TODO
+        try{
+            Statement statement = minConnection.createStatement();
+            ResultSet res = statement.executeQuery("EXEC getCompanyNameByEmployee " + emp.getEmployeeId());
+            res.next();
+            Cname = res.getString(1);
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
         return Cname;
     }
     //return id of company for employee
